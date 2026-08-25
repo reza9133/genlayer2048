a23ba0fd4a5e9d3702aa57b6c5115eebb28b2e75
amirhossein <119801602+reza9133@users.noreply.github.com>
2026-08-26 00:09:30 +0330

    Update genlayer.ts

--- src/genlayer.ts
+++ src/genlayer.ts
@@ -9,7 +9,7 @@ import type {
 } from "./types";
 
 export const CONTRACT_ADDRESS = (import.meta.env.VITE_CONTRACT_ADDRESS ||
-  "0xc77dfa45968464ef7ef9d83bD7eeeC158377978f") as `0x${string}`;
+  "0x63281F0EA13A77f8d31ba3eA38D90c45f0Ea135B") as `0x${string}`;
 
 export const CHAIN = testnetBradbury;
 export const NETWORK_NAME = "testnetBradbury" as const;
